33c15c5fa7b1645a9793781c8f3d790ac88902ce
nbaez <nbaez01@hotmail.com>
2019-01-06 14:49:45 -0500

    First Commit

---
+++ src/main/java/com/empresa/app/CursoSpringApplication.java
@@ -0,0 +1,14 @@
+package com.empresa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+}
+
